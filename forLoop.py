d8c929b3f7d324ebc25420b514419b0b7fa43714
Paredesh26 <sophiahays55@gmail.com>
2025-02-11 10:25:17 -0500

    for Loop Examples

---
+++ forLoop.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 27 13:34:32 2025
+
+@author: paredesh3418
+"""
+'''
+names = ['Jessica', 'Tom', 'Sandy']
+
+for x in names: 
+    
+    print("Good morning,", x)
+'''
+'''
+nums = [5,7,6,10]
+
+print(f'{"Nums":<5}{"sqr"}')
+print("-"*10)
+
+for x in nums:
+    
+    sqr = x**2
+    print(f'{x:<5}{sqr}')
+'''
+
+'''
+print(f'{"Nums":<5}{"sqr"}')
+print("-"*10)
+
+for x in range(5,25):
+    
+    sqr = x**2
+    print(f'{x:<5}{sqr}')
+'''
+
+'''    
+print(f'{"Nums":<5}{"sqr"}')
+print("-"*10)
+
+for x in range(5,26,5):
+    
+    sqr = x**2
+    print(f'{x:<5}{sqr}')
+'''
+
+print(f'{"Nums":<5}{"sqr"}')
+print("-"*10)
+
+for x in range(25,4,-5):
+    
+    sqr = x**2
+    print(f'{x:<5}{sqr}')
